e8dc950b389e91e16544df8f0820fa8109968b41
falling_jimmy <clown_87@inbox.ru>
2021-05-15 23:03:43 +0300

    '4'

--- currency_task.py
+++ currency_task.py
@@ -12,48 +12,75 @@ class CurrencyAnalizer:
 		self.day_numbers = day_numbers
 		self.date_values = {}
 
-	def data_structure(self):
-		date = datetime.date.today()
-		for i in range(self.day_numbers):
-			self.date_values[str(date)] = {}
-			y = str(date.year)
-			m = str(date.month)
-			d = str(date.day)
-			if len(m) < 2:
-				m = '0' + m
-			if len(d) < 2:
-				d = '0' + d
-			params = {'date_req': f'{d}/{m}/{y}'}
-			res = requests.get(self.url, params=params)
-			root = ElementTree.fromstring(res.text)
-			for child in root:
-				self.date_values[str(date)][child[3].text] = float(child[4].text.replace(",", "."))
-			date = date - datetime.timedelta(1)
-		return self.date_values
+		def data_structure(self):
+			date = datetime.date.today()
+			for i in range(self.day_numbers):
+				self.date_values[str(date)] = {}
+				y = str(date.year)
+				m = str(date.month)
+				d = str(date.day)
+				if len(m) < 2:
+					m = '0' + m
+				if len(d) < 2:
+					d = '0' + d
+				params = {'date_req': f'{d}/{m}/{y}'}
+				res = requests.get(self.url, params=params)
+				root = ElementTree.fromstring(res.text)
+				for child in root:
+					self.date_values[str(date)][child[3].text] = float(child[4].text.replace(",", "."))
+				date = date - datetime.timedelta(1)
+			return self.date_values
+		data_structure(self)
 
 
-	# def find_minimum(self):
-	# 	min = float('inf')
-	# 	min_dict = {}
-	# 	data_dict = 
-	# 	for date in data_dict.keys():
-	# 		for currency_name in data_dict[date].keys():
-	# 			if currency_name in min_dict:
-	# 				if data_dict[currency_name] < min_dict[currency_name]:
-	# 					min_dict[currency_name] = data_dict[currency_name]
-	# 			else:
-	# 				min_dict[currency_name] = data_dict[currency_name]
-	# 	return min_dict
+	def find_minimum(self):
+		min = float('inf')
+		min_dict = {}
+		data_dict = self.date_values
+		date_minimum = {}
+		for date in data_dict.keys():
+			for currency_name in data_dict[date].keys():
+				if currency_name in min_dict:
+					if data_dict[date][currency_name] < min_dict[currency_name]:
+						min_dict[currency_name] = data_dict[date][currency_name]
+						date_minimum[currency_name] = date
+				else:
+					min_dict[currency_name] = data_dict[date][currency_name]
+					date_minimum[currency_name] = date
+		return min_dict, date_minimum
 
+	def find_maximum(self):
+		max = 0
+		max_dict = {}
+		date_maximum = {}
+		data_dict = self.date_values
+		for date in data_dict.keys():
+			for currency_name in data_dict[date].keys():
+				if currency_name in max_dict:
+					if data_dict[date][currency_name] > max_dict[currency_name]:
+						max_dict[currency_name] = data_dict[date][currency_name]
+						date_maximum[currency_name] = date
+				else:
+					max_dict[currency_name] = data_dict[date][currency_name]
+					date_maximum[currency_name] = date
+		return max_dict, date_maximum
 
-
-
+	def find_average(self):
+		average_dict = {}
+		data_dict = self.date_values
+		count = 0
+		for date in data_dict.keys():
+			count += 1
+			for currency_name in data_dict[date].keys():
+				if currency_name in average_dict:
+					average_dict[currency_name] += data_dict[date][currency_name]
+				else:
+					average_dict[currency_name] = data_dict[date][currency_name]
+		for key in average_dict.keys():
+			average_dict[key] /= count
+		return average_dict
 		
 
-	# def find_maximum(self):
-	# 	pass
 
-	# def find_average(self):
-	# 	pass
-
-
+test_object = CurrencyAnalizer(day_numbers=50)
+print(test_object.find_maximum())
