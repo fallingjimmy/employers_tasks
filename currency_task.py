bfd22b79ed473853db12741cadecf392f09454a1
falling_jimmy <clown_87@inbox.ru>
2021-05-15 20:29:24 +0300

    '2'

--- currency_task.py
+++ currency_task.py
@@ -1,64 +1,46 @@
-impif len(m) < 2:
-		m = '0' + m
-	if len(d) < 2:
-		d = '0' + dimport datetime
 import requests
-from bs4 import BeautifulSoup
+import datetime
 from xml.etree import ElementTree
 
-class CurrencyDate:
-	def __init__(self, date):
-		self.date = date
-		self.currency_value_dict ={}
+class CurrencyAnalizer:
+	def __init__(self, day_numbers=90):
+		# self.d = str()
+		# self.m = str()
+		# self.y = str()
+		self.url = 'http://www.cbr.ru/scripts/XML_daily_eng.asp'
+		#self.params = {'date_req': f'{self.d}/{self.m}/{self.y}'}
+		self.day_numbers = day_numbers
+		self.date_values = {}
+
+	def data_structure(self):
+		date = datetime.date.today()
+		for i in range(self.day_numbers):
+			self.date_values[date] = {}
+			y = str(date.year)
+			m = str(date.month)
+			d = str(date.day)
+			if len(m) < 2:
+				m = '0' + m
+			if len(d) < 2:
+				d = '0' + d
+			params = {'date_req': f'{d}/{m}/{y}'}
+			res = requests.get(self.url, params=params)
+			root = ElementTree.fromstring(res.text)
+			for child in root:
+				self.date_values[date][child[3].text] = float(child[4].text.replace(",", "."))
+			date = date - datetime.timedelta(1)
+		return self.date_values
 
 
-	def append_rate(self, currency, value):
-		self.currency_value_dict[currency] = value
+	def find_minimum(self):
+		pass
 
-	def __repr__(self):
-		return f'({self.date}, {self.currency_value_dict})'
+	def find_maximum(self):
+		pass
 
-
-def main():
-	date = datetime.date.today()
-	currency_exchange_rate = []
-	currency_name = []
-
-	for i in range(90):
-		y = str(date.year)
-		m = str(date.month)
-		d = str(date.day)
-		if len(m) < 2:
-			m = '0' + m
-		if len(d) < 2:
-			d = '0' + d
-		day_object = CurrencyDate(date)
-		url = 'http://www.cbr.ru/scripts/XML_daily_eng.asp'
-		params = {'date_req': f'{d}/{m}/{y}'}
-		res = requests.get(url, params=params)
-		root = ElementTree.fromstring(res.text)
-		for child in root:
-			day_object.append_rate(child[3].text, child[4].text)
-		currency_exchange_rate.append(day_object)
-		date = date - datetime.timedelta(1)
-	return currency_exchange_rate
-
-xml_list = main()
-for i in xml_list:
-	
+	def find_average(self):
+		pass
 
 
-
-
-			
-			
-
-
-
-
-
-
-
-
-
-
+test_object = CurrencyAnalizer()
+print(test_object.data_structure())
